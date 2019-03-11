1ca718b305e8c592780901f08500c0adb2fd0672
Michael Schwartz <mykesx@gmail.com>
2019-03-11 08:04:03 -0700

    convert Theater.js to hooks

--- client/src/screens/Theater.js
+++ client/src/screens/Theater.js
@@ -1,47 +1,41 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import Config from "Config";
 
 import { Tab, Tabs } from "react-bootstrap";
 import TheaterTab from "tabs/TheaterTab";
 
-export default class Theater extends Component {
-  constructor(props) {
-    super(props);
-    this.changeTab = this.changeTab.bind(this);
-    this.state = {
-      activeTab: localStorage.getItem("theaterTabState") || "0"
-    };
-  }
-  changeTab(eventKey) {
+export default () => {
+  const [activeTab, setActiveTab] = useState(
+    localStorage.getItem("theaterTabState") || "0"
+  );
+  const changeTab = eventKey => {
     localStorage.setItem("theaterTabState", eventKey);
-    this.setState({ activeTab: eventKey });
-  }
-  render() {
-    const activeTab = this.state.activeTab;
-    return (
-      <Tabs
-        id="theater-tabs"
-        onSelect={this.changeTab}
-        activeKey={activeTab}
-        bsStyle="pills"
-        mountOnEnter
-        unmountOnExit
-      >
-        {Config.theaters.map(theater => {
-          //          console.log("theater", theater);
-          return (
-            <Tab
-              title={theater.title}
-              eventKey={theater.key}
-              key={theater.key}
-              style={{ paddingLeft: 10, paddingRight: 10 }}
-            >
-              <TheaterTab theater={theater} />
-            </Tab>
-          );
-        })}
-      </Tabs>
-    );
-  }
-}
+    setActiveTab(eventKey);
+  };
+
+  return (
+    <Tabs
+      id="theater-tabs"
+      onSelect={changeTab}
+      activeKey={activeTab}
+      bsStyle="pills"
+      mountOnEnter
+      unmountOnExit
+    >
+      {Config.theaters.map(theater => {
+        //          console.log("theater", theater);
+        return (
+          <Tab
+            title={theater.title}
+            eventKey={theater.key}
+            key={theater.key}
+            style={{ paddingLeft: 10, paddingRight: 10 }}
+          >
+            <TheaterTab theater={theater} />
+          </Tab>
+        );
+      })}
+    </Tabs>
+  );
+};
