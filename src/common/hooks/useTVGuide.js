9d38aedefda2e2fc1516dee76a96a598507f6b68
Michael Schwartz <mykesx@gmail.com>
2019-05-22 12:20:05 -0700

    add useTVGuide

---
+++ src/common/hooks/useTVGuide.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import MQTT from "lib/MQTT";
+import Config from "Config";
+
+const useTVGuide = guide => {
+  const [channels, setChannels] = useState(null);
+
+  useEffect(() => {
+    const handleChannels = (topic, message) => {
+      setChannels(message);
+    };
+    MQTT.subscribe(
+      `${Config.mqtt.tvguide}/${guide}/status/channels`,
+      handleChannels
+    );
+    return () => {
+      MQTT.unsubscribe(
+        `${Config.mqtt.tvguide}/${guide}/status/channels`,
+        handleChannels
+      );
+    };
+  }, []);
+
+  return {
+    channels: channels
+  };
+};
+
+export default useTVGuide;
--- src/common/hooks/useWeather.js
+++ src/common/hooks/useWeather.js
@@ -1,4 +1,4 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import MQTT from "lib/MQTT";
 import Config from "Config";
 
