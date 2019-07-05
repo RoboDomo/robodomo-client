8964c75942ad1368f4835a83ac78b37b50235672
Sergiu Popescu <sergiu@moduscreate.com>
2019-07-05 11:33:23 +0300

    Remove unnecessary examples. Move e2e-tests folder Write the first RoboDomo test

--- src/e2e-tests/examples/wdio/jasmine/jasmine.spec.js
+++
@@ -1,7 +0,0 @@
-describe('webdriver.io page', () => {
-    it('should have the right title', () => {
-        browser.url('https://webdriver.io')
-        const title = browser.getTitle()
-        expect(title).toBe('WebdriverIO · Next-gen WebDriver test framework for Node.js')
-    })
-})
