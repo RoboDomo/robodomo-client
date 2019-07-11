fbb1dce95fd9db50eb54f3e19af0e740e7c165c0
Igor Protsenko <ig.protsenko@gmail.com>
2019-07-11 11:25:58 +0300

    WDIO config update to match new directory name

--- src/e2e-tests/configs/wdio.conf.js
+++ src/e2e-tests/configs/wdio.conf.js
@@ -130,7 +130,7 @@ exports.config = {
     ],
     // If you are using Cucumber you need to specify the location of your step definitions.
     cucumberOpts: {
-        require: ['./step-definitions/*.js'],        // <string[]> (file/dir) require files before executing features
+        require: ['./step_definitions/*.js'],        // <string[]> (file/dir) require files before executing features
         backtrace: false,    // <boolean> show full backtrace for errors
         compiler: [],        // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
         dryRun: false,       // <boolean> invoke formatters without executing steps
