f2ef83d36bb4d4b7849bdaf11adebd9cc9b4a015
BuildTools <unconfigured@null.spigotmc.org>
2019-07-12 11:29:42 -0400

    ButtonGroup to IonSegment

--- src/common/form/ToggleField.js
+++ src/common/form/ToggleField.js
@@ -1,35 +1,33 @@
 import React from "react";
-import { ButtonToolbar, ButtonGroup, Button } from "react-bootstrap";
+import { IonSegment, IonSegmentButton, IonLabel } from "@ionic/react"
 
 const ToggleField = ({ name, label, toggled, onToggle }) => {
   return (
     <div style={{ display: "flex", marginTop: 10 }}>
       <div style={{ marginTop: 12, flex: 1 }}>{label}</div>
       <div className="float-right" style={{ whiteSpace: "nowrap" }}>
-        <ButtonToolbar>
-          <ButtonGroup>
-            <Button
-              variant={toggled ? "dark" : undefined}
-              onClick={() => {
-                if (onToggle && !toggled) {
-                  onToggle(name, true);
-                }
-              }}
-            >
-              On
-            </Button>
-            <Button
-              variant={toggled ? undefined : "dark"}
-              onClick={() => {
-                if (onToggle && toggled) {
-                  onToggle(name, false);
-                }
-              }}
-            >
-              Off
-            </Button>
-          </ButtonGroup>
-        </ButtonToolbar>
+        <IonSegment>
+          <IonSegmentButton 
+            checked={toggled ? true : undefined} 
+            onClick={() => {
+              if (onToggle && !toggled) {
+                onToggle(name, true);
+              }
+            }}
+          >
+            <IonLabel>On</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton 
+            checked={toggled ? undefined : true} 
+            onClick={() => {
+              if (onToggle && toggled) {
+                onToggle(name, false);
+              }
+            }}
+          >
+            <IonLabel>Off</IonLabel>
+          </IonSegmentButton>
+        </IonSegment>
       </div>
     </div>
   );
