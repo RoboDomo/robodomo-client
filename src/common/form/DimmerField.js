adaf2b98b33a08bef667a0e9831123a9ae0c830d
BuildTools <unconfigured@null.spigotmc.org>
2019-07-12 11:28:55 -0400

    Toggle to IonToggle

--- src/common/form/DimmerField.js
+++ src/common/form/DimmerField.js
@@ -1,8 +1,7 @@
 import React /*, { useState, useEffect, useRef }*/ from "react";
 
 import s from "./DimmerField.module.css";
-import { IonRange, IonIcon} from '@ionic/react';
-import Toggle from "react-bootstrap-toggle";
+import { IonRange, IonIcon, IonToggle } from '@ionic/react';
 
 const DimmerField = ({ label, name, value, toggled, onToggle, onValueChange }) => {
   const applyValueChange = val => {
@@ -27,7 +26,7 @@ const DimmerField = ({ label, name, value, toggled, onToggle, onValueChange }) =
     <div style={{ display: "flex", marginTop: 10 }}>
       <div style={{ marginTop: 10, flex: 1 }}>{label}</div>
       <div style={{ whiteSpace: "nowrap", display: "flex" }}>
-        <Toggle active={toggled} onClick={handleToggle} style={{ flex: 1, marginRight: 20 }} />
+        <IonToggle checked={toggled} onClick={handleToggle} className={s.toggle}/>
         <div style={{ flex: 1, marginTop: 0 }}>
           <IonRange
             className = {s.range}
