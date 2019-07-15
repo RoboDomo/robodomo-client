9da9b82d80ded485d4ef7f6b9b6c939b6a44cce9
Grgur Grisogono <grgur@grgur.com>
2019-07-15 18:20:31 +0200

    Weather sub-tab

--- src/Tablet/Weather/Weather.js
+++ src/Tablet/Weather/Weather.js
@@ -1,43 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
+import { Route } from "react-router";
+import { IonTabs, IonTabBar, IonTabButton, IonLabel, IonRouterOutlet } from "@ionic/react";
 import { IonContent } from "@ionic/react";
 import useConfig from "@/hooks/useConfig";
 
-import { Tab, Tabs } from "react-bootstrap";
 import WeatherTab from "./WeatherTab";
 
 const Weather = () => {
   const Config = useConfig();
-  const [activeTab, setActiveTab] = useState(localStorage.getItem("weatherTabState") || "0");
   if (!Config) {
     return null;
   }
-  const changeTab = eventKey => {
-    localStorage.setItem("weatherTabState", eventKey);
-    setActiveTab(eventKey);
-  };
   return (
     <IonContent id="tab-weather">
-      <Tabs
-        id="weather-tabs"
-        onSelect={changeTab}
-        activeKey={activeTab}
-        variant="pills"
-        mountOnEnter
-        unmountOnExit
-      >
-        {Config.weather.locations.map(location => {
-          return (
-            <Tab
-              title={location.name}
-              eventKey={location.name}
-              key={location.name}
-              style={{ paddingLeft: 10, paddingRight: 10 }}
-            >
-              <WeatherTab location={location} />
-            </Tab>
-          );
-        })}
-      </Tabs>
+      <IonTabs>
+        <IonRouterOutlet>
+          {Config.weather.locations.map(({ device: zip }) => (
+            <Route path={`/weather/:tab(${zip})`} component={WeatherTab} key={zip} />
+          ))}
+        </IonRouterOutlet>
+        <IonTabBar slot="bottom">
+          {Config.weather.locations.map(({ device: zip, name }) => (
+            <IonTabButton tab={zip} href={`/weather/${zip}`} key={zip}>
+              <IonLabel>{name}</IonLabel>
+            </IonTabButton>
+          ))}
+        </IonTabBar>
+      </IonTabs>
     </IonContent>
   );
 };
