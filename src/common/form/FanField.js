546430c346667ff87e0c8cb0837bc14c2190c7e8
chrndr <32408212+chrndr@users.noreply.github.com>
2019-07-15 03:26:06 -0400

    Replaced various React bootstrap components with Ionic components (#5)
    * slider to Ionic component
    
    * moved slider style to CSS module
    
    * toggle to Ionic component
    
    * undo add IonToggle, fix event handling
    
    * added 100ms delay before slider update
    
    * ButtonGroup to IonSegment
    
    * ButtonGroup to IonSegment, add css module
    
    * Buttons, input to Ionic component
    
    * matched ion text color to other text
    
    * success/danger colors
    
    * Toggle to IonToggle
    
    * ButtonGroup to IonSegment

--- src/common/form/FanField.js
+++ src/common/form/FanField.js
@@ -2,37 +2,37 @@
 
 import React from "react";
 
-import { ButtonGroup, Button } from "react-bootstrap";
+import { IonSegment, IonSegmentButton, IonLabel } from "@ionic/react"
 
 const FanField = ({ label, name, toggled, value, onChange }) => {
   if (!name) {
     throw new Error("FanField name prop required");
   }
 
-  const styles = {};
-  styles.on = toggled ? "primary" : "dark";
-  styles.low = toggled && value <= 33 ? "dark" : "primary";
-  styles.medium = toggled && value <= 66 && value > 33 ? "dark" : "primary";
-  styles.high = toggled && value > 66 ? "dark" : "primary";
+  const checked = {};
+  checked.off = !toggled;
+  checked.low = toggled && value <= 33;
+  checked.medium = toggled && value <= 66 && value > 33;
+  checked.high = toggled && value > 66;
 
   return (
     <div style={{ display: "flex", marginTop: 10 }}>
       <div style={{ flex: 1, marginTop: 12 }}>{label}</div>
       <div style={{ textAlign: "right", marginRight: 25 }}>
-        <ButtonGroup>
-          <Button variant={styles.on} onClick={() => onChange(name, "off")}>
-            Off
-          </Button>
-          <Button variant={styles.low} onClick={() => onChange(name, "low")}>
-            Low
-          </Button>
-          <Button variant={styles.medium} onClick={() => onChange(name, "medium")}>
-            Medium
-          </Button>
-          <Button variant={styles.high} onClick={() => onChange(name, "high")}>
-            High
-          </Button>
-        </ButtonGroup>
+        <IonSegment>
+          <IonSegmentButton checked={checked.off} onClick={() => onChange(name, "off")}>
+            <IonLabel>Off</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton checked={checked.low} onClick={() => onChange(name, "low")}>
+            <IonLabel>Low</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton checked={checked.medium} onClick={() => onChange(name, "medium")}>
+            <IonLabel>Medium</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton checked={checked.high} onClick={() => onChange(name, "high")}>
+            <IonLabel>High</IonLabel>
+          </IonSegmentButton>
+        </IonSegment>
       </div>
     </div>
   );
