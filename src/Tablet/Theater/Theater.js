e0755fdb092c2710ef541fc9f66c5b9556c1263f
Grgur Grisogono <grgur@grgur.com>
2019-07-16 17:51:15 +0200

    Tabbed view template

--- src/Tablet/Theater/Theater.js
+++ src/Tablet/Theater/Theater.js
@@ -1,50 +1,15 @@
 import React from "react";
-import { Route, Redirect } from "react-router";
-import {
-  IonTabs,
-  IonTabBar,
-  IonTabButton,
-  IonLabel,
-  IonRouterOutlet,
-  IonContent,
-} from "@ionic/react";
-import useConfigGroup from "@/hooks/useConfigGroup";
+import TabbedView from "@/templates/TabbedView";
 
 import TheaterTab from "./TheaterTab";
 
-const Theater = () => {
-  const [theaters, defaultTheater] = useConfigGroup("theaters");
+const Theater = () => (
+  <TabbedView
+    id="tab-theaters"
+    section="theaters"
+    route="theater"
+    render={data => <TheaterTab theater={data} />}
+  />
+);
 
-  if (!theaters) {
-    return null;
-  }
-
-  return (
-    <IonContent id="tab-dashboard">
-      <IonTabs>
-        <IonRouterOutlet>
-          {theaters.map(theater => (
-            <Route
-              path={`/theater/:tab(${theater.key})`}
-              render={() => <TheaterTab theater={theater} />}
-              key={theater.key}
-            />
-          ))}
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          {theaters.map(theater => (
-            <IonTabButton tab={theater.key} href={`/theater/${theater.key}`} key={theater.key}>
-              <IonLabel>{theater.title}</IonLabel>
-            </IonTabButton>
-          ))}
-        </IonTabBar>
-      </IonTabs>
-      <Route
-        exact
-        path="/theater"
-        render={() => <Redirect to={`/theater/${defaultTheater.key}`} />}
-      />
-    </IonContent>
-  );
-};
 export default Theater;
