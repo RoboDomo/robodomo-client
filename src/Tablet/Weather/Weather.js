dc34f9466bc7cb7f9e458614f3cbe9cd455eb14f
Grgur Grisogono <grgur@grgur.com>
2019-07-16 18:02:42 +0200

    weather page

--- src/Tablet/Weather/Weather.js
+++ src/Tablet/Weather/Weather.js
@@ -1,46 +1,17 @@
 import React from "react";
-import { Route, Redirect } from "react-router";
-import { IonTabs, IonTabBar, IonTabButton, IonLabel, IonRouterOutlet } from "@ionic/react";
-import { IonContent } from "@ionic/react";
-import useConfigGroup from "@/hooks/useConfigGroup";
+import TabbedView from "@/templates/TabbedView";
 
 import WeatherTab from "./WeatherTab";
 
-const Weather = () => {
-  const [locations, defaultLocation] = useConfigGroup("weather.locations");
+const Weather = () => (
+  <TabbedView
+    id="tab-weather"
+    section="weather.locations"
+    route="weather"
+    sectionKey="device"
+    titleKey="name"
+    render={data => <WeatherTab zip={data.device} />}
+  />
+);
 
-  if (!locations) {
-    return null;
-  }
-
-  return (
-    <IonContent id="tab-weather">
-      <IonTabs>
-        <IonRouterOutlet>
-          {locations.map(({ device: zip }) => (
-            <Route
-              path={`/weather/:tab(${zip})`}
-              render={() => <WeatherTab zip={zip} />}
-              key={zip}
-            />
-          ))}
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          {locations.map(({ device: zip, name }) => (
-            <IonTabButton tab={zip} href={`/weather/${zip}`} key={zip}>
-              <IonLabel>{name}</IonLabel>
-            </IonTabButton>
-          ))}
-        </IonTabBar>
-      </IonTabs>
-      <Route
-        exact
-        path="/weather"
-        render={() => <Redirect to={`/weather/${defaultLocation.device}`} />}
-      />
-    </IonContent>
-  );
-};
-
-//
 export default Weather;
