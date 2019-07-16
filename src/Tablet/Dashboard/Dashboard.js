53214696ea952383478f92ea0686ad92a9087a4d
andrewtran30 <51962465+andrewtran30@users.noreply.github.com>
2019-07-16 16:05:08 -0400

    Update Dashboard.js

--- src/Tablet/Dashboard/Dashboard.js
+++ src/Tablet/Dashboard/Dashboard.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { IonContent } from "@ionic/react";
 
-
 import useConfig from "@/hooks/useConfig";
 
 import { Tab, Tabs } from "react-bootstrap";
@@ -14,11 +13,6 @@ const Dashboard = () => {
     return null;
   }
 
-  if (activeTab == null) {
-    console.log("THERE IS NO ACTIVE TAB");
-  } else {
-    console.log("THIS IS THE ACTIVE TAB: ", activeTab);
-  }
   //console.log("THIS IS THE SET ACTIVE TAB: ", setActiveTab)
   const changeTab = eventKey => {
     localStorage.setItem("dashboardTabState", eventKey);
