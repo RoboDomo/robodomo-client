7d717e9f0ba6bf084ae3b09d320f717361a7c235
Grgur Grisogono <grgur@grgur.com>
2019-07-22 18:37:35 +0200

    Persist demo state (#29)
    Demo state will persist in IDB. The database doesn’t get cleared at any point so persistence is permanent on the browser instance.
    
    Closes #26

--- config-overrides.js
+++ config-overrides.js
@@ -77,6 +77,7 @@ module.exports = function override(config) {
             "react-popper",
             "react-icons",
             "framer-motion",
+            "idb-keyval",
             // cannot DLL ionic due to resolver issues in AutoDLL plugin
           ],
         },
