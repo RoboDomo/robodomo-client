0aaad6440f482c3021a6ae6e08ab954fdad9fce4
Grgur Grisogono <grgur@grgur.com>
2019-07-24 17:36:57 +0200

    autelis

--- src/Tablet/Autelis/Autelis.js
+++ src/Tablet/Autelis/Autelis.js
@@ -1,40 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { IonContent } from "@ionic/react";
-import useConfig from "@/hooks/useConfig";
-
-import { Tabs, Tab } from "react-bootstrap";
-
 import AutelisTab from "./AutelisTab";
 
-const LOCALSTORAGE_KEY = "autelisTabletTabState";
-
-const Autelis = () => {
-  const Config = useConfig();
-  const [activeTab, setActiveTab] = useState(localStorage.getItem(LOCALSTORAGE_KEY) || "0");
-
-  if (!Config) {
-    return null;
-  }
-
-  return (
-    <IonContent id="tab-autelis">
-      <Tabs
-        id="autelis-tabs"
-        onSelect={eventKey => {
-          localStorage.setItem(LOCALSTORAGE_KEY, eventKey);
-          setActiveTab(eventKey);
-        }}
-        activeKey={activeTab}
-        variant="pills"
-        mountOnEnter
-        unmountOnExit
-      >
-        <Tab title={Config.autelis.device.toUpperCase()} eventKey="autelis" key="autelis">
-          <AutelisTab />
-        </Tab>
-      </Tabs>
-    </IonContent>
-  );
-};
+const Autelis = () => (
+  <IonContent id="tab-sensors">
+    <AutelisTab />
+  </IonContent>
+);
 
 export default Autelis;
