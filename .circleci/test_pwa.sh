13db397c9515ea008473172f601a254cf11e1d72
Madalin Borodi <borodi.madalin@gmail.com>
2019-07-25 12:46:42 +0300

    Fixing file

--- .circleci/test_pwa.sh
+++ .circleci/test_pwa.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yarn --cwd ./e2e-tests run test:bs_cloud_smoke --baseUrl=http://localhost:3000 --user=${BS_USER} --key=${BS_KEY} --cucumberOpts.tagExpression='@automated and @smoke'
+yarn --cwd ./e2e-tests run test:bs_cloud_smoke --baseUrl="http://localhost:3000" --user=${BS_USER} --key=${BS_KEY} --cucumberOpts.tagExpression='@automated and @smoke'
