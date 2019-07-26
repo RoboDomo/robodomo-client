26ca0846421e7c9e1d8f73e11ee290ed7b9d502d
Sergiu Popescu <sergiu@moduscreate.com>
2019-07-26 16:49:17 +0300

    Add more tests to smoke pack

--- e2e-tests/configs/wdio.conf.bs.cloud.smoke.js
+++ e2e-tests/configs/wdio.conf.bs.cloud.smoke.js
@@ -2,7 +2,7 @@ const merge = require('deepmerge');
 const configBase = require('./wdio.conf.bs.cloud');
 
 exports.config = merge(configBase.config, {
-    specs: ['./features/smoke/*.feature'],
+    specs: ['./features/**/*.feature'],
 
     capabilities: [
         {
