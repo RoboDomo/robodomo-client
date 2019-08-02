37bb523043dc940460c0a56a699916a08452f28e
Grgur Grisogono <grgur@grgur.com>
2019-08-02 17:06:13 +0200

    Show theater devices (#58)

--- src/Tablet/Theater/TheaterDevice.js
+++ src/Tablet/Theater/TheaterDevice.js
@@ -7,7 +7,7 @@ import LGTVControl from "./LGTVControl";
 import BraviaControl from "./BraviaControl";
 import DenonControl from "./DenonControl";
 
-const TheaterDevice = ({ currentDevice, avr, tv, deviceMap }) => {
+const TheaterDevice = ({ currentDevice, deviceMap }) => {
   if (!currentDevice) {
     return <h1>All Off</h1>;
   }
@@ -22,9 +22,11 @@ const TheaterDevice = ({ currentDevice, avr, tv, deviceMap }) => {
     case "AppleTV":
       return <AppleTVControl device={deviceMap.appletv.device} />;
     case "LG TV":
+    case "TVLG":
       // TODO tvInput, avrInput props might change?  Use key...
       return <LGTVControl config={deviceMap.lgtv} />;
     case "TV":
+    case "TVSONY":
       return <BraviaControl config={deviceMap.bravia} />;
     default:
       return <h1>All Off</h1>;
