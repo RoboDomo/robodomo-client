845636f06e8b10831fb1986fff421da0bfed20dd
Grgur Grisogono <grgur@grgur.com>
2019-08-07 10:53:15 +0200

    Fix module type in workers

--- config-overrides.js
+++ config-overrides.js
@@ -86,7 +86,7 @@ module.exports = function override(config) {
     );
 
     // Web Workers plugin
-    config.plugins.push(new WorkerPlugin());
+    config.plugins.push(new WorkerPlugin({ preserveTypeModule: true }));
   }
 
   return config;
